4402e67afa60ae1f09f5c0d21e90f3a01f1f8596
JeremiahYueWang <jeremiah.yue.wang@gmail.com>
2014-05-15 17:04:37 +0800

    添加用户信息界面，利用interface来实现异步下载后的startactivity工作

---
+++ WebClient/src/com/example/webclient/ShowUserInfo.java
@@ -0,0 +1,16 @@
+package com.example.webclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.webclient.R;
+
+/**
+ * Created by Aozaki_Shiro on 5/15/2014.
+ */
+public class ShowUserInfo extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_user_info);
+    }
+}
---
+++ WebClient/src/com/example/webclient/WaitingForResult.java
@@ -0,0 +1,7 @@
+package com.example.webclient;
+
+/**
+ * Created by Aozaki_Shiro on 5/15/2014.
+ */
+public interface WaitingForResult {
+}
