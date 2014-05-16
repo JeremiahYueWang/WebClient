b223d5dbb2e3e65b754ca0277c73095c3c8f37cd
JeremiahYueWang <jeremiah.yue.wang@gmail.com>
2014-05-16 19:32:22 +0800

    no message

---
+++ WebClient/src/com/example/protocol/Config.java
@@ -0,0 +1,7 @@
+package com.example.protocol;
+
+/**
+ * Created by Aozaki_Shiro on 5/16/2014.
+ */
+public class Config {
+}
---
+++ WebClient/src/com/example/webclient/ImageUtil.java
@@ -0,0 +1,7 @@
+package com.example.webclient;
+
+/**
+ * Created by Aozaki_Shiro on 5/16/2014.
+ */
+public class ImageUtil {
+}
